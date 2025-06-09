1fe34cc212b0e8d43115c542fde0ff819061c376
LogisticaIncobel <logistica@incobel.com.br>
2025-06-09 09:31:51 -0300

    Update mapa.js

--- public/mapa.js
+++ public/mapa.js
@@ -1,23 +1,22 @@
-let map = L.map('map').setView([-23.5505, -46.6333], 13); // Centralizando no Brasil
+let map = L.map('map').setView([-23.5505, -46.6333], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '© OpenStreetMap'
 }).addTo(map);
 
-fetch('./localizacoes.json')  // URL da sua API no GitHub Pages
+fetch('localizacoes.json')  // <- carrega o JSON da mesma pasta
     .then(response => response.json())
     .then(data => {
-        data.forEach(caminhao => {
-            let marker = L.marker([caminhao.latitude, caminhao.longitude]).addTo(map);
-            marker.bindPopup(`
-                <b>Placa: ${caminhao.placa}</b><br>
-                Data: ${caminhao.data}<br>
-                Hora: ${caminhao.hora}<br>
-                Latitude: ${caminhao.latitude}<br>
-                Longitude: ${caminhao.longitude}<br>
-                Velocidade: ${caminhao.velocidade}<br>
-                Ignicão: ${caminhao.ignicao}<br>
-                <a href="https://link_para_historico">Histórico de 72h</a>
-            `);
+        data.forEach(c => {
+            L.marker([c.latitude, c.longitude])
+              .addTo(map)
+              .bindPopup(`
+                <b>Placa: ${c.placa}</b><br>
+                Data: ${c.data}<br>
+                Hora: ${c.hora}<br>
+                Velocidade: ${c.velocidade}<br>
+                Ignição: ${c.ignicao}<br>
+                Lat: ${c.latitude}, Long: ${c.longitude}
+              `);
         });
     });
