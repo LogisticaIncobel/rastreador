74e46c017904de799474c80c60abcfa960610bf3
LogisticaIncobel <logistica@incobel.com.br>
2025-06-09 09:25:30 -0300

    Update mapa.js

--- public/mapa.js
+++ public/mapa.js
@@ -4,7 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-fetch('https://LogisticaIncobel.github.io/rastreador/api/localizacoes')  // URL da sua API no GitHub Pages
+fetch('./localizacoes.json')  // URL da sua API no GitHub Pages
     .then(response => response.json())
     .then(data => {
         data.forEach(caminhao => {
